71600226ac1efad1dcd7655c623228045d953b1c
Catur Wicaksono <bitcodenetwork@gmail.com>
2025-01-15 11:09:37 +0700

    update test

---
+++ tests/container.test.ts
@@ -0,0 +1,9 @@
+import Docker from "../src";
+
+test('get list container', () => {
+  const docker = new Docker();
+
+  const containers = docker.container();
+
+  expect(containers).toBeTruthy();
+});
